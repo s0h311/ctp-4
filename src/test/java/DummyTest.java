2e563f279c3e1897ad102b07bd9bb1893cf29181
Soheil <soheil.nazari@check24.de>
2024-05-28 10:43:22 +0200

    add test

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+  @Test
+  public void dummyTest() {
+    Assertions.assertTrue(true);
+  }
+}
